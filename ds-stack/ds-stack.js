b453c20b7b82fef5a9f3ddd5e4e7b10c70722ca7
ENGMENJARI <anjari1981@gmail.com>
2017-01-07 09:50:17 +0200

    stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,23 @@
 var Stack = function() {
 	//your code is here
+	var storage=[]
+	var count=0
+	storage.push=function(value){
+		
+		storage[count]=value
+		count++
+	}
+	storage.pop=function(){
+		var temp=storage[count]
+		if(storage[count]!==undefined){
+		storage.pop()
+		count--
+		
+	}
+		return temp
+	}
+	storage.size=function(){
+		return count
+	}
+	return storage
 };
